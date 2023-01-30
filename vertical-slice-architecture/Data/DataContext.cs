1173dba4489345c5d5ef51a48ee3d43f8a945080
KTECH\Cihan <c.tekin@keesingtechnologies.com>
2023-01-30 23:09:38 +0100

    create datacontext and use inmemory db

---
+++ vertical-slice-architecture/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using vertical_slice_architecture.Domain;
+
+namespace vertical_slice_architecture.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Television> Televisions { get; set; }
+    }
+}
--- vertical-slice-architecture/Program.cs
+++ vertical-slice-architecture/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using vertical_slice_architecture.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("TvDb"));
 
 var app = builder.Build();
 
