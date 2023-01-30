1a21a2d20e3331a3d2138a35b0e0775bd4bf110e
KTECH\Cihan <c.tekin@keesingtechnologies.com>
2023-01-31 00:03:12 +0100

    Brands seed

---
+++ vertical-slice-architecture/Data/Seed.cs
@@ -0,0 +1,30 @@
+﻿namespace vertical_slice_architecture.Data
+{
+    public class Seed
+    {
+        public void SeedData(DataContext dataContext)
+        {
+            // Brands
+            dataContext.Brands.Add(new Domain.Brand
+            {
+                Id = 1,
+                Name = "Samsung",
+                Origin = "South Korean"
+            });
+
+            dataContext.Brands.Add(new Domain.Brand
+            {
+                Id = 2,
+                Name = "LG",
+                Origin = "South Korean"
+            });
+
+            dataContext.Brands.Add(new Domain.Brand
+            {
+                Id = 2,
+                Name = "Sony",
+                Origin = "Japanese"
+            });
+        }
+    }
+}
