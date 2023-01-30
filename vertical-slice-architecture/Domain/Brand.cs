99e8d3267269e78cb1f7377b084f9278f8344797
KTECH\Cihan <c.tekin@keesingtechnologies.com>
2023-01-30 23:03:40 +0100

    domain models

---
+++ vertical-slice-architecture/Domain/Brand.cs
@@ -0,0 +1,9 @@
+﻿namespace vertical_slice_architecture.Domain
+{
+    public class Brand
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Origin { get; set; }
+    }
+}
---
+++ vertical-slice-architecture/Domain/Television.cs
@@ -0,0 +1,12 @@
+﻿namespace vertical_slice_architecture.Domain
+{
+    public class Television
+    {
+        public int Id { get; set; }
+        public string Model { get; set; }
+
+        public int BrandId { get; set; }
+        public Brand Brand { get; set; }
+        public decimal Inch { get; set; }
+    }
+}
