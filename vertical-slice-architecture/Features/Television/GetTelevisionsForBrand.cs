75062ac94173c5e6ad9a7961719512620dfacaa5
KTECH\Cihan <c.tekin@keesingtechnologies.com>
2023-02-01 21:15:28 +0100

    Television Query

---
+++ vertical-slice-architecture/Features/Television/GetTelevisionsForBrand.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+
+namespace vertical_slice_architecture.Features.Television
+{
+    public class GetTelevisionsForBrand
+    {
+        public class GetTvQuery : IRequest<IEnumerable<TvResult>>
+        {
+            public int BrandId { get; set; }
+        }
+
+        public class TvResult
+        {
+            public int Id { get; set; }
+            public string Model { get; set; }
+            public int BrandId { get; set; }
+            public decimal Inch { get; set; }
+        }
+
+        public class Handler : IRequestHandler<GetTvQuery, IEnumerable<TvResult>>
+        {
+            public Task<IEnumerable<TvResult>> Handle(GetTvQuery request, CancellationToken cancellationToken)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
--- vertical-slice-architecture/Features/Television/TelevisionController.cs
+++ vertical-slice-architecture/Features/Television/TelevisionController.cs
@@ -1,5 +1,6 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using static vertical_slice_architecture.Features.Television.GetTelevisionsForBrand;
 
 namespace vertical_slice_architecture.Features.Television
 {
@@ -10,6 +11,26 @@ namespace vertical_slice_architecture.Features.Television
         public TelevisionController(IMediator mediator)
         {
             _mediator = mediator;
+        }
+
+        public async Task<ActionResult<IEnumerable<TvResult>>> GetTelevisionsForBrand(int brandId)
+        {
+            try
+            {
+                var query = new GetTvQuery
+                {
+                    BrandId = brandId
+                };
+
+                var result = await _mediator.Send(query); 
+                
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
         }
     }
 }
