0ba8ed752db87907c8467c5eb006da8ca34a71ef
KTECH\Cihan <c.tekin@keesingtechnologies.com>
2023-02-03 22:23:34 +0100

    Brand Feature - Get All method

---
+++ vertical-slice-architecture/Features/Brand/BrandController.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using static vertical_slice_architecture.Features.Brand.GetBrands;
+
+namespace vertical_slice_architecture.Features.Brand
+{
+    public class BrandController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public BrandController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("GetAllBrands")]
+        public async Task<ActionResult<IEnumerable<BrandResult>>> GetAllBrands()
+        {
+            var result = await _mediator.Send(new GetBrandQuery());
+
+            if (result == null)
+                return NotFound();
+
+            return Ok(result);
+        }
+    }
+}
---
+++ vertical-slice-architecture/Features/Brand/GetBrands.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+
+namespace vertical_slice_architecture.Features.Brand
+{
+    public class GetBrands
+    {
+        public class GetBrandQuery : IRequest<IEnumerable<BrandResult>> { }
+        public class BrandResult
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Origin { get; set; }
+        }
+
+        public class Handler : IRequestHandler<GetBrandQuery, IEnumerable<BrandResult>>
+        {
+            public Task<IEnumerable<BrandResult>> Handle(GetBrandQuery request, CancellationToken cancellationToken)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+    }
+}
