0716bc98955d805c84ec27df5fa919cda9488fa9
KTECH\Cihan <c.tekin@keesingtechnologies.com>
2023-02-04 22:33:52 +0100

    AddTelevisionCommand

---
+++ vertical-slice-architecture/Features/Television/AddTelevision.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using static vertical_slice_architecture.Features.Television.GetTelevisionsForBrand;
+
+namespace vertical_slice_architecture.Features.Television
+{
+    public class AddTelevision
+    {
+        public class AddTelevisionCommand : IRequest<TvResult>
+        {
+            public int Id { get; set; }
+            public string Model { get; set; }
+            public int BrandId { get; set; }
+            public decimal Inch { get; set; }
+        }
+
+        public class Handler : IRequestHandler<AddTelevisionCommand, TvResult>
+        {
+            public Task<TvResult> Handle(AddTelevisionCommand request, CancellationToken cancellationToken)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
