3c232de413a4932778c03417fcbf17d518719aa8
KTECH\Cihan <c.tekin@keesingtechnologies.com>
2023-02-04 22:38:59 +0100

    AddTelevision service method

--- vertical-slice-architecture/Features/Television/ITelevisionService.cs
+++ vertical-slice-architecture/Features/Television/ITelevisionService.cs
@@ -1,7 +1,9 @@
-﻿namespace vertical_slice_architecture.Features.Television
+﻿
+namespace vertical_slice_architecture.Features.Television
 {
     public interface ITelevisionService
     {
         Task<IEnumerable<Domain.Television>> GetTelevisionsForBrand(int brandId);
+        Task AddTelevision(Domain.Television television);
     }
 }
--- vertical-slice-architecture/Features/Television/TelevisionService.cs
+++ vertical-slice-architecture/Features/Television/TelevisionService.cs
@@ -12,6 +12,11 @@ namespace vertical_slice_architecture.Features.Television
             _dataContext = dataContext;
         }
 
+        public async Task AddTelevision(Domain.Television television)
+        {
+            await _dataContext.AddAsync(television);
+        }
+
         public async Task<IEnumerable<Domain.Television>> GetTelevisionsForBrand(int brandId)
         {
             return await _dataContext.Televisions.Where(x => x.BrandId == brandId).ToListAsync();
