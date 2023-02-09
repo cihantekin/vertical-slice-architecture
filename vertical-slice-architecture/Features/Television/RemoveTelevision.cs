7b5494ddd8678f98c4aa85e21edbb086a2c01c3d
KTECH\Cihan <c.tekin@keesingtechnologies.com>
2023-02-09 20:27:13 +0100

    implementing tv removal

--- vertical-slice-architecture/Features/Television/RemoveTelevision.cs
+++ vertical-slice-architecture/Features/Television/RemoveTelevision.cs
@@ -8,5 +8,22 @@ namespace vertical_slice_architecture.Features.Television
         {
             public int TelevisionId { get; set; }
         }
+
+        public class Handler : IRequestHandler<RemoveTelevisionCommand, Unit>
+        {
+            private readonly ITelevisionService _televisionService;
+
+            public Handler(ITelevisionService televisionService)
+            {
+                _televisionService = televisionService;
+            }
+
+            //TODO: implement auto mapper and handle null cases
+            public Task<Unit> Handle(RemoveTelevisionCommand request, CancellationToken cancellationToken)
+            {
+                _televisionService.RemoveTelevision(new Domain.Television { Id = request.TelevisionId });
+                return Unit.Task;
+            }
+        }
     }
 }
