4f7d6e6721953e67b71321b0ca4950383e0e9879
KTECH\Cihan <c.tekin@keesingtechnologies.com>
2023-02-10 23:13:42 +0100

    NoTelevisionExistException

---
+++ vertical-slice-architecture/Features/Television/Exceptions/NoGameExistException.cs
@@ -0,0 +1,8 @@
+﻿namespace vertical_slice_architecture.Features.Television.Exceptions
+{
+    public class NoTelevisionExistException : Exception
+    {
+        public NoTelevisionExistException() : base($"No Televisions was found!") { }
+        public NoTelevisionExistException(int televisionId) : base($"No Television was found with id: {televisionId}") { }
+    }
+}
