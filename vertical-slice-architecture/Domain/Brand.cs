8b261b8d2171549718129713dc790f8122daad26
KTECH\Cihan <c.tekin@keesingtechnologies.com>
2023-02-15 23:22:08 +0100

    warnings

--- vertical-slice-architecture/Domain/Brand.cs
+++ vertical-slice-architecture/Domain/Brand.cs
@@ -3,7 +3,7 @@
     public class Brand
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Origin { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Origin { get; set; } = string.Empty;
     }
 }
--- vertical-slice-architecture/Domain/Television.cs
+++ vertical-slice-architecture/Domain/Television.cs
@@ -3,10 +3,10 @@
     public class Television
     {
         public int Id { get; set; }
-        public string Model { get; set; }
+        public string Model { get; set; } = string.Empty;
 
         public int BrandId { get; set; }
-        public Brand Brand { get; set; }
+        public Brand Brand { get; set; } = new();
         public decimal Inch { get; set; }
     }
 }
