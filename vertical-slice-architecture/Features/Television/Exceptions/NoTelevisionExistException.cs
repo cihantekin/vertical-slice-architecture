daa97c727e502639abfacc3ad6ee1853145c388b
KTECH\Cihan <c.tekin@keesingtechnologies.com>
2023-02-16 23:08:26 +0100

    removed unused exception class

--- vertical-slice-architecture/Features/Television/Exceptions/NoTelevisionExistException.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace vertical_slice_architecture.Features.Television.Exceptions
-{
-    public class NoTelevisionExistException : Exception
-    {
-        public NoTelevisionExistException() : base($"No Televisions was found!") { }
-        public NoTelevisionExistException(int brandId) : base($"No Television was found with brand id: {brandId}") { }
-    }
-}
